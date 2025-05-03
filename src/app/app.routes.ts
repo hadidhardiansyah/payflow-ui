650376e908e0715bffde21f18b4d01381ba38825
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-05-04 05:59:35 +0700

    Create blank dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { AUTH_ROUTES } from './modules/auth/auth.routes';
+import { DASHBOARD_ROUTES } from './modules/dashboard/dahsboard.routes';
 
 export const routes: Routes = [
   {
     path: 'auth',
     children: AUTH_ROUTES,
+  },
+  {
+    path: 'dashboard',
+    children: DASHBOARD_ROUTES,
   },
   {
     path: '',
