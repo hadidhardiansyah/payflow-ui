d2614d727af615a772f89c8c96f1674c43164b66
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-05-05 17:45:27 +0700

    Given condition to arrow icon dropdown

--- src/app/modules/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/modules/dashboard/components/sidebar/sidebar.component.ts
@@ -1,19 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-  isSidebarOpen = false;
+  isSidebarOpen: boolean = false;
+  isTrxDropdownOpen: boolean = false;
+  isTaskDropdownOpen: boolean = false;
 
   constructor(private eRef: ElementRef) {}
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  toggleTrxDropdown() {
+    this.isTrxDropdownOpen = !this.isTrxDropdownOpen;
+  }
+
+  toggleTaskDropdown() {
+    this.isTaskDropdownOpen = !this.isTaskDropdownOpen;
   }
 
   @HostListener('document:click', ['$event'])
